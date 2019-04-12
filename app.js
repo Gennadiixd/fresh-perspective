cf5a3dd62335039799984078deac55e37e110789
Gennadiixd <gennadiixd@gmail.com>
2019-04-12 14:24:22 +0300

    added favicon

--- app.js
+++ app.js
@@ -82,6 +82,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+app.use('/favicon.ico', express.static('favicon.ico'));
+
 
 // Подключаем импортированные маршруты с определенным url префиксом.
 app.use('/', indexRouter);
